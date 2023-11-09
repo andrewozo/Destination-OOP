b677ff7b76db243029c9801900187172ce8de581
Magnan <sylmar956@gmail.com>
2023-11-09 16:57:32 -0500

    Plane test and class

--- classes/Plane.js
+++ classes/Plane.js
@@ -1,5 +1,23 @@
 class Plane {
 
+    static origin="JFK"
+
+    constructor(company,destination){
+        this.company=company
+        this.destination=destination
+        this.passengers=[]
+
+    }
+
+    getPassengers(){
+        return this.passengers
+    }
+
+    addPassenger(passenger){
+        this.passengers.push(passenger)
+    }
+
+
 
 }
 
